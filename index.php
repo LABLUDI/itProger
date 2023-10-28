9db66d68c1c15a8b4b94d1a2c77c4a366d911f95
AlexL <chuiflame@gmail.com>
2023-10-28 18:52:28 +0200

    String operations

--- index.php
+++ index.php
@@ -9,38 +9,15 @@
 </head>
 <body>
 <?php
-    $x = 10;
-    $y = 20;
+    $str = 'Hello';
+    echo "VAR: $str" . '!' . '<br>';
+    echo "<input type='text'>" . '<br>';
 
-    echo $x + $y . '<br>';
-    echo $x - $y . '<br>';
-    echo $x * $y . '<br>';
-    echo $x / $y . '<br>';
-    echo $x % $y . '<br>';
+    $length = strlen($str);
+    echo strtoupper(trim('  sdf  ')) . '<br>';
+    echo $length . '<br>';
 
-    $x += 10; // $X = $x + 10;
-    $y -= 10; // $y = $y - 10;
-    echo $x . '<br>';
-    echo $y . '<br>';
-
-    $x++;// $x += 1; Инкремент
-    $x--; // $x -= 1; Декремент
-
-    echo M_PI . '<br>';
-    echo M_E . '<br>';
-
-    echo abs(-22) . '<br>';
-    echo ceil(3.1) . '<br>';
-    echo floor(3.9) . '<br>';
-    echo round(3.15435353, 2) . '<br>';
-
-    $rand = mt_rand(1, 974982108708999999);
-    $rand++;
-
-    echo $rand . '<br>';
-    echo min(2,0, 5, 4, 3, 6, 24, 41, 4, 3) . '<br>';
-    echo max(2,0, 5, 4, 3, 6, 24, 41, 4, 3) . '<br>';
-
+    echo md5('27011999') . '<br>';
 ?>
 </body>
 </html>
