e86a4a82cb3e14feb452c2f0fef45d44c5c87350
AlexL <chuiflame@gmail.com>
2023-10-30 12:52:58 +0200

    Creating files, connecting files

---
+++ about.php
@@ -0,0 +1,9 @@
+<?php
+$title = 'О нас';
+
+require 'blocks/header.php';
+?>
+<h1>О нас</h1>
+<?php
+require 'blocks/footer.php';
+?>
--- index.php
+++ index.php
@@ -1,64 +1,8 @@
-<!doctype html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport"
-	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>PHP</title>
-</head>
-<body>
 <?php
-    function info($word) {
-        echo "$word<br>";
-    }
-    info(1);
-    info('Hello');
-
-    function math($x, $y) {
-        return $x + $y;
-    }
-
-    $res = math(4,6);
-    $res2 = math(90, 90000);
-
-    info($res);
-    info($res2);
-
-    function summary($arr){
-        $summa = 0;
-        foreach($arr as $value){
-            $summa += $value;
-        }
-        return $summa;
-    }
-
-    $list = [5, 7, 3];
-    echo summary($list).'<br>';
-    echo summary([5, 2, 3]) . '<br>';
-
-    // Область видимости
-
-    function info2() {
-        global $n;
-        $n = 0;
-    }
-
-
-    function  click() {
-        static $count;
-        $count++;
-        echo $count . '<br>';
-    }
-
-    $n = 6780;
-    info2();
-    echo $n;
-    click();
-    click();
-    click();
-    click();
-    click();
+    $title = 'Главная страница';
+    require_once 'blocks/header.php';
 ?>
-</body>
-</html>
+<h1>Главная страница</h1>
+<?php
+    require_once 'blocks/footer.php';
+?>
