fb6b981b196513c170e50da61b55f6c8518f8007
AlexL <chuiflame@gmail.com>
2023-10-31 21:42:09 +0200

    Working with date and time

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
 ?>
 <h1>Главная страница</h1>
 <?php
-    print_r($_GET);
+//    echo date('m-l H:i:s', time() + 10000) . '<br>';
+
+//    echo date('m-d H:i:s', strtotime('-1 Week -2 Day -3 Hour'));
+
+    echo date('m-d H:i:s', strtotime('Next Monday'));
+
     require_once 'blocks/footer.php';
 ?>
