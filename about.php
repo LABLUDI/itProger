4e090ceea930954f5e216806433c9e03246dc7d5
AlexL <chuiflame@gmail.com>
2023-10-31 12:53:28 +0200

    Обработка форм. Передача данных методом POST и GET, страница для обработки даннх, написание проверок, кэширование пароля, переадресация пользователя

--- about.php
+++ about.php
@@ -3,7 +3,17 @@ $title = 'О нас';
 
 require 'blocks/header.php';
 ?>
-<h1>О нас</h1>
+    <div class="container mt-2">
+        <h1>О нас</h1>
+        <form action="check_get.php" method="get">
+            <input type="text" name="username" placeholder="Введите имя" class="form-control"><br>
+            <input type="email" name="email" placeholder="Введите email" class="form-control"><br>
+            <input type="password" name="password" placeholder="Введите пароль" class="form-control"><br>
+            <textarea name="message" class="form-control" placeholder="Введите сообщение" id="" cols="10"
+                      rows="5"></textarea><br>
+            <input type="submit" value="Отпрвить" class="btn btn-success">
+        </form>
+    </div>
 <?php
 require 'blocks/footer.php';
 ?>
--- index.php
+++ index.php
@@ -4,5 +4,6 @@
 ?>
 <h1>Главная страница</h1>
 <?php
+    print_r($_GET);
     require_once 'blocks/footer.php';
 ?>
