f9567ad78f77837835b2e83d6be4294d70d06e47
AlexL <chuiflame@gmail.com>
2023-11-02 18:00:50 +0200

    Sending mail from the site

--- index.php
+++ index.php
@@ -4,18 +4,15 @@ require_once 'blocks/header.php';
 ?>
 	<h1>Главная страница</h1>
 <?php
-if(isset($_GET['source']) && $_GET['source'] !== "") {
-    $link = explode("?source=", $_SERVER['REQUEST_URI']);
-    $redirect = "http://" . $_SERVER['HTTP_HOST'] . $link[0];
-    header('HTTP/1.1 301 Moved Permanently');
-    header('Location: '. $redirect);
-    exit();
-}
-//	phpinfo();
-//echo '<pre>';
-//echo $_SERVER['HTTP_HOST'] . ' ~ ' . $_SERVER['REQUEST_URI'];
-//echo $_SERVER['HTTP_USER_AGENT'];
-//echo '</pre>';
+    $message = 'Сообщение';
+    $to = 'admin@itproger.com';
+    $from = 'example@itproger.com';
+    $subject = 'Тема сообщения';
+
+    $subject = '=?utf-8?b?' . base64_encode($subject) . '?=';
+    $headers = "From: $from\r\nReply-to: $from\r\nContent-type:text/plane; charset=utf-8\r\n";
+
+    mail($to, $subject, $message, $headers);
 
 require_once 'blocks/footer.php';
 ?>
