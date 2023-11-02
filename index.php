33a9c97f24a183938cbe4110139447b9f88f4c8f
AlexL <chuiflame@gmail.com>
2023-11-02 12:38:38 +0200

    Функция phpinfo() и массив $_SERVER

--- index.php
+++ index.php
@@ -2,36 +2,20 @@
 $title = 'Главная страница';
 require_once 'blocks/header.php';
 ?>
-    <h1>Главная страница</h1>
+	<h1>Главная страница</h1>
 <?php
-//    $file = fopen('text.txt', 'a');
-//
-//    fwrite($file, "\nExample text \nHello");
-//
-//    fclose($file);
-
-    $filename = 'text.txt';
-
-    $file = fopen('text.txt', 'r');
-
-    $content = fread($file, filesize($filename));
-
-    fclose($file);
-
-    echo '<pre>' . $content . '</pre>' . '<br>';
-
-    file_put_contents('a.txt', "Example\nHello");
-
-    echo file_get_contents('a.txt') . '<br>';
-
-    echo file_exists('a.txt') . '<br>';
-
-//    rename('a.txt', 'new_name.txt');
-
-//    unlink('new_name.txt');
-
-    echo fileperms(__FILE__);
-    chmod(__FILE__, 777);
+if(isset($_GET['source']) && $_GET['source'] !== "") {
+    $link = explode("?source=", $_SERVER['REQUEST_URI']);
+    $redirect = "http://" . $_SERVER['HTTP_HOST'] . $link[0];
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: '. $redirect);
+    exit();
+}
+//	phpinfo();
+//echo '<pre>';
+//echo $_SERVER['HTTP_HOST'] . ' ~ ' . $_SERVER['REQUEST_URI'];
+//echo $_SERVER['HTTP_USER_AGENT'];
+//echo '</pre>';
 
 require_once 'blocks/footer.php';
 ?>
