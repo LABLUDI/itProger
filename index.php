ee94c1b4d989ecae971594f3145748a5046d4a87
AlexL <chuiflame@gmail.com>
2023-11-02 10:35:20 +0200

    Internal php functions

--- index.php
+++ index.php
@@ -1,14 +1,63 @@
 <?php
-    $title = 'Главная страница';
-    require_once 'blocks/header.php';
+$title = 'Главная страница';
+require_once 'blocks/header.php';
 ?>
-<h1>Главная страница</h1>
+    <h1>Главная страница</h1>
 <?php
-//    echo date('m-l H:i:s', time() + 10000) . '<br>';
+$lis = [
+	5,
+	7,
+	3,
+	6,
+	7,
+	8,
+];
+unset($lis[1]);
+$lis = array_values($lis);
+//    sort($lis);
+//    rsort($lis);
+//    shuffle($lis);
 
-//    echo date('m-d H:i:s', strtotime('-1 Week -2 Day -3 Hour'));
+//    if(in_array(30, $lis) == '')
+//        echo 'Not found';
+//    else
+//        echo 'Found';
 
-    echo date('m-d H:i:s', strtotime('Next Monday'));
+$arr = array_slice($lis, 2, 2);
+var_dump($arr);
+echo '<br>';
 
-    require_once 'blocks/footer.php';
+$arr_1 = [
+	5,
+	7,
+];
+$arr_2 = [
+	6,
+	8,
+	9,
+];
+$arr_3 = array_merge($arr_1, $arr_2);
+
+print_r($arr_3);
+echo '<br>';
+
+print_r($lis);
+echo '<br>';
+
+$x = (float)'10';
+echo gettype($x) . '<br>';
+echo is_numeric($x) . '<br>';
+
+$str = 'Example';
+echo strpos($str, 'am') . '<br>';
+
+$words = 'jhon, bob, alex';
+$arr_words = explode(',', $words);
+print_r($arr_words);
+echo '<br>';
+echo '<br>' . implode(' ', $arr_words);
+
+
+
+require_once 'blocks/footer.php';
 ?>
